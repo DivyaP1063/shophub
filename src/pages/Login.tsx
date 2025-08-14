cb60369e8613bd97aaf582127016b6c70884c259
DivyaP1063 <divyaprakash1063@gmail.com>
2025-08-14 16:35:09 +0530

    minor changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -85,13 +85,14 @@ const Login = () => {
             <div className="flex-grow h-px bg-gray-200" />
           </div>
 
-          <div className="w-full google-signin-full-width">
+
+          <div className="w-full google-signin-full-width flex justify-center group ">
             <GoogleLogin
               onSuccess={handleGoogleLogin}
               onError={() => console.log("Google Login Failed")}
               shape="rect"
               size="large"
-              theme="filled_blue"
+              theme="filled_white"
               width="100%" // Ask Google for full width
             />
           </div>
