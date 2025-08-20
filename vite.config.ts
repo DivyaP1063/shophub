c690a8d9b377a80b4e25e5230bc720a4b6afb9bc
DivyaP1063 <divyaprakash1063@gmail.com>
2025-08-20 23:12:40 +0530

    landding and e-commerce merged

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,11 @@ export default defineConfig(({ mode }) => ({
   publicDir: 'public', // <-- ensures public/_redirects is copied to dist
   plugins: [
     react(),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      deleteOriginFile: false,
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
